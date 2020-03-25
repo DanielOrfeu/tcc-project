956ce957e4041ac1e170055da949eec8a31e14ee
Thiago Lemos <thiagolemos1996@gmail.com>
2020-03-24 21:30:55 -0300

    criação de funções para alterar o style do app

--- src/app/pages/config/config.component.ts
+++ src/app/pages/config/config.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, HostBinding} from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-config',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./config.component.css']
 })
 export class ConfigComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private sanitizer: DomSanitizer) { }
+  
   ngOnInit(): void {
   }
 
+
+  @HostBinding("attr.style")
+  changeBackgroundColor(color:string){
+    document.body.style.setProperty('background-color', color);
+  }
+
+  changeFontColor(color:string){
+    document.body.style.setProperty('color', color);
+  }
+
+  changeFontSize(size:string){
+    console.log(size)
+    document.body.style.setProperty('font-size', size);
+  }
 }
