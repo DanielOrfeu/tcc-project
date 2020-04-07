f09e4e2e8e19674a658e253cc17742aa9bca1b47
Daniel Orfeu <daniel_yago1997@hotmail.com>
2020-04-06 23:45:03 -0300

    Constants e ajustes inGame

---
+++ src/app/constants/mode.constant.ts
@@ -0,0 +1,28 @@
+import { NumbersEXP } from "./numbers.constant"
+
+export const Modes = [
+    {
+        title: "Expressão aritimética",
+        specs: NumbersEXP
+    },
+    {
+        title: "Expressão linguística",
+        // specs: PortugueseEXP
+    },
+    {
+        title: "Formas geométricas",
+        // specs: GeometricsEXP
+    },
+    {
+        title: "Jogo Misto",
+        // specs: MixEXP
+    }
+]
+
+
+
+
+
+
+
+
